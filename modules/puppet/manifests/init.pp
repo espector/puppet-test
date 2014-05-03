52f67ff10c00127f40c23fa238fe843533107a32
Eric Spector <eric.w.spector@gmail.com>
2014-05-03 04:31:05 +0000

    added cron

--- modules/puppet/manifests/init.pp
+++ modules/puppet/manifests/init.pp
@@ -3,4 +3,20 @@ class puppet {
            source => 'puppet:///modules/puppet/papply.sh',
            mode   => '0755',
          }
+	file { '/usr/local/bin/pull-updates':
+           source => 'puppet:///modules/puppet/pull-updates.sh',
+           mode   => '0755',
+	}
+         file { '/home/vagrant/.ssh/id_rsa.pub':
+           source => 'puppet:///modules/puppet/centos.pub',
+           owner  => 'ubuntu',
+           mode   => '0600',
+	}
+         cron { 'run-puppet':
+           ensure  => 'present',
+           user    => 'vagrant',
+           command => '/usr/local/bin/pull-updates',
+           minute  => '*/10',
+	   hour => '*', 
+	}
 }
